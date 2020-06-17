f299fd71f6dba723c7bffb02c165d81b8c2b78bf
Magdalena Czajka <magdalena.czajka94@gmail.com>
2020-06-17 19:08:54 +0200

    add support for flask forms

--- alegrosz/__init__.py
+++ alegrosz/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 
 def create_app():
     alegrosz = Flask(__name__)
+    alegrosz.config['SECRET_KEY'] = b'S\xdfCf\xee\xf0\x8d\xf2\xfb\xdaWA"6\xc4\x8f'
+
 
     from alegrosz.views.main_views import main_bp
 
